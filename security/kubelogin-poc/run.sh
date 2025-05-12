58ccb33b138019df58690149eba8e15fc221404e
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2025-05-12 13:53:37 +0300

    add PKCE option to kubelogin POC
    Add device-code flow with PKCE to Kubelogin POC.
    
    Signed-off-by: Tuomo Tanskanen <tuomo.tanskanen@est.tech>

--- security/kubelogin-poc/run.sh
+++ security/kubelogin-poc/run.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
-KIND_NAME="poc"
+TYPE="${1:?type missing: either 'password' or 'device-code'}"
+
+KIND_NAME="kubelogin-poc"
 KUBELOGIN_CACHE="${HOME}/.kube/cache/oidc-login"
 
+echo "checking tools ..."
 command -v kubectl &>/dev/null || { echo "error: kubectl not in PATH"; exit 1; }
 command -v kind &>/dev/null || { echo "error: kind not in PATH"; exit 1; }
 
+echo "removing old clusters with name '${KIND_NAME}' ..."
 rm -rf "${KUBELOGIN_CACHE}"
 kind delete cluster --name "${KIND_NAME}" || true
+
+echo "generating certificates ..."
 rm -f ssl/* && ./gencert.sh
+
+echo "creating kind cluster with name '${KIND_NAME}' ..."
 kind create cluster --config=kind.conf --name "${KIND_NAME}"
 sleep 20
-kubectl apply -f k8s/dex.yaml
+
+echo "applying dex-${TYPE}.yaml ..."
+kubectl apply -f "k8s/dex-${TYPE}.yaml"
 kubectl create secret tls dex.example.com.tls \
     --cert=ssl/cert.pem --key=ssl/key.pem --namespace=dex
+
+echo "applying openldap.yaml ..."
 kubectl apply -f k8s/openldap.yaml
 
+echo "creating cluster roles ..."
 # create role for user "customuser"
 # user is whatever the group-claim=groups field defines groups field is
 kubectl create clusterrolebinding oidc-cluster-admin \
@@ -32,6 +45,38 @@ kubectl create clusterrolebinding oidc-view-group \
 sleep 20
 kubectl get pods -A
 
+echo "configuring kubekonfig for user oidc ..."
+if [[ "${TYPE}" == "password" ]]; then
+    kubectl config set-credentials oidc \
+        --exec-api-version=client.authentication.k8s.io/v1beta1 \
+        --exec-command=kubectl \
+        --exec-arg=oidc-login \
+        --exec-arg=get-token \
+        --exec-arg=--oidc-issuer-url=https://dex.example.com:32000 \
+        --exec-arg=--oidc-client-id=kubelogin-test \
+        --exec-arg=--oidc-extra-scope=email \
+        --exec-arg=--oidc-extra-scope=profile \
+        --exec-arg=--oidc-extra-scope=groups \
+        --exec-arg=--insecure-skip-tls-verify \
+        --exec-arg=--oidc-client-secret=kubelogin-test-secret \
+        --exec-arg=--grant-type=password \
+        --exec-arg=-v1
+else
+    kubectl config set-credentials oidc \
+        --exec-api-version=client.authentication.k8s.io/v1beta1 \
+        --exec-command=kubectl \
+        --exec-arg=oidc-login \
+        --exec-arg=get-token \
+        --exec-arg=--oidc-issuer-url=https://dex.example.com:32000 \
+        --exec-arg=--oidc-client-id=kubelogin-test \
+        --exec-arg=--oidc-extra-scope=email \
+        --exec-arg=--oidc-extra-scope=profile \
+        --exec-arg=--oidc-extra-scope=groups \
+        --exec-arg=--insecure-skip-tls-verify \
+        --exec-arg=--oidc-pkce-method=S256 \
+        --exec-arg=--grant-type=device-code \
+        --exec-arg=-v1
+fi
 cat <<EOF
 Done!
 
